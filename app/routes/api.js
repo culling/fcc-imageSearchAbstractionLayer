23091434dca0c4f01354904ec0016db76a283e2d
culling <culling66@gmail.com>
2017-05-12 13:08:28 +0800

    Basic API guide written as well

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,16 +5,20 @@ const querystring   = require('querystring');
 var express         = require('express');
 var router          = express.Router();
 
-
-var config  = require("./../../config/config");
-
+//Configs and Modules
+var config      = require("./../../config/config");
 var mongoExport = require("./../../config/mongo");
 
 
 router.get("/", function(req, res){
-    res.write("API Query");
-    res.end();
+    //res.write("API Query");
+    //res.write("Guide to API");
+    //res.write('<a href=/')
+    //res.end();
+    res.sendFile(("apiguide.html"), {root: "public"});
+
 });
+
 
 router.post("/images/new", function(req, res){
     var body = req.body;
@@ -33,13 +37,6 @@ router.post("/images/new", function(req, res){
 
 //Allowing for the query to be modified as a get request
 router.get("/images/search/new", function(req, res){
-    //console.log(req.body["search-text"] );
-    //var searchText = req.body["search-text"];
-    /*
-    var queryParts = req.url.split("?");
-    var query = queryParts[queryParts.length -1 ];
-    var searchText = query.split("search-text=")[query.split("search-text=").length -1];
-    */
     var reqQuery    = querystring.parse(url.parse(req.url).query);
     var searchText  = reqQuery["search-text"];
     var offset      = reqQuery["offset"] || 0;
@@ -84,24 +81,5 @@ router.get("/images/search", function(req, res){
     
 });
 
-/*
-router.post("/search/", function(req, res){
-    //console.log(req.body["search-text"] );
-    var reqQuery    = querystring.parse(url.parse(req.url).query);
-    var searchText  = reqQuery["search-text"];
-    var offset      = reqQuery["offset"];
-    console.log("offset: " + offset);
-
-    
-    mongoExport.search.retrieve( searchText , 0, function(foundDocs ){
-        foundDocs.forEach(function(foundDoc){
-            //console.log(foundDoc);
-            res.write(JSON.stringify(foundDoc) + "\n");
-        });
-        //res.write( JSON.stringify(req.body));
-        res.end();
-    }); 
-});
-*/
 
 module.exports = router;

4a35cfe96f9e41d7f08d93f212b8ced39e806b11
culling <culling66@gmail.com>
2017-05-12 13:44:40 +0800

    pt2

--- app/models/search.model.server.js
+++ app/models/search.model.server.js
@@ -5,7 +5,6 @@ var mongo               =   require("mongodb").MongoClient;
 var mongoPort           = config.mongoPort;
 var mongoDatabase       = config.mongoDatabase;
 var collectionName      = "search";
-var mongoMaxReturnedDocs = 10;
 //console.log(collectionName);
 var mongoUrl =  `mongodb://localhost:${mongoPort}/${mongoDatabase}`;
 
@@ -39,6 +38,7 @@ exports.retrieve = function(offset, res){
         var collection = db.collection(collectionName);
 
         collection.find(query).sort({"submitted": -1}).skip(limit * offset).limit(limit).toArray(function (err, results){
+            if(err){console.error(err)}
         //collection.find({}).toArray(function (err, results){
             if (results.length > 0){
                 console.log(results );
